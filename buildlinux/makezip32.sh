1973ab363ebfb72c6535b34e2ee1a3dbe09ffd64
Jeremy Whiting <jpwhiting@kde.org>
2017-01-21 12:10:13 -0700

    Added scripts to quickly create zip files after debian packages are installed.

---
+++ buildlinux/makezip32.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Before executing this script make sure you have the 32 bit debian package installed.
+cat files.txt | zip safejumper-linux-32.zip -@
---
+++ buildlinux/makezip64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Before executing this script make sure you have the 64 bit debian package installed.
+cat files.txt | zip safejumper-linux-64.zip -@
