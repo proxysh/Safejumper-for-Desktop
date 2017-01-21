47c39043f866bcb72b6835780b1f2becd752a99e
Jeremy Whiting <jpwhiting@kde.org>
2017-01-21 12:13:52 -0700

    Updated debian package version.
    Added debianpackages.sh helper to run both debian package scripts.
    Added cleanup.sh to clean up build files.
    Added builddebian64.sh to build 64 bit debian package.

---
+++ buildlinux/cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -fR launchopenvpn safejumper netdown
---
+++ buildlinux/debianpackages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VERSION=2017.01.20
+QTVERSION=5.7.0
+OLDPATH=$PATH
+export PATH=../../qt-$QTVERSION-32bit/qtbase/bin:$OLDPATH
+./builddebian32.sh $VERSION
+./cleanup.sh
+export PATH=../../qt-$QTVERSION-64bit/qtbase/bin:$OLDPATH
+./builddebian64.sh $VERSION
+./cleanup.sh
+export PATH=$OLDPATH
