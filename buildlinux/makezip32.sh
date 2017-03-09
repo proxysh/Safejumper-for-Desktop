9ea6996c466b0b9fcbe2ae7521b235ed2f1cbff2
Jeremy Whiting <jpwhiting@kde.org>
2017-03-08 18:49:29 -0700

    Make makezip scripts add all of /opt/safejumper recursively.

--- buildlinux/makezip32.sh
+++ buildlinux/makezip32.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Before executing this script make sure you have the 32 bit debian package installed.
 cat files.txt | zip safejumper-linux-32.zip -@
+zip -r safejumper-linux-32.zip /opt/safejumper 
--- buildlinux/makezip64.sh
+++ buildlinux/makezip64.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Before executing this script make sure you have the 64 bit debian package installed.
 cat files.txt | zip safejumper-linux-64.zip -@
+zip -r safejumper-linux-64.zip /opt/safejumper 
