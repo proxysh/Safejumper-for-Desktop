12813e6dcb628734e5b05b44f3e9f6e35a8d4ee4
Jeremy Whiting <jpwhiting@kde.org>
2017-07-18 16:00:21 -0600

    Linux: If ifconfig is not found use ip to disable network devices.

--- src/netdown/main.cpp
+++ src/netdown/main.cpp
@@ -2,6 +2,7 @@
 #include "common.h"
 #include "runit.h"
 
+#include <QFile>
 #include <QStringList>
 
 int main(int argc, char *argv[])
@@ -12,10 +13,14 @@ int main(int argc, char *argv[])
         "/sbin/ifconfig en1 down" <<
         "/usr/sbin/networksetup -setairportpower Wi-Fi off";
 #else
-    commands << "/sbin/ifconfig eth0 down" <<
-        "/sbin/ifconfig eth1 down";
+    if (QFile::exists("/sbin/ifconfig")) {
+        commands << "/sbin/ifconfig eth0 down" <<
+            "/sbin/ifconfig eth1 down";
+    } else {
+        // No ifconfig, so use ip
+        commands << "/sbin/ip link set group default down";
+    }
 #endif
-
     become_root();
 
     for (int k = 0; k < commands.size(); ++k)
