94ae1389ff806ec9e513d2ae0962fa8330108755
Jeremy Whiting <jpwhiting@kde.org>
2018-11-30 19:29:35 -0700

    Update makezip scripts for shieldtra.
    Also commit shieldtra.desktop file.

--- buildlinux/makezip32.sh
+++ buildlinux/makezip32.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Before executing this script make sure you have the 32 bit debian package installed.
-cat files.txt | zip safejumper-linux-32.zip -@
-zip -r safejumper-linux-32.zip /opt/safejumper 
+cat files.txt | zip shieldtra-linux-32.zip -@
+zip -r shieldtra-linux-32.zip /opt/shieldtra
--- buildlinux/makezip64.sh
+++ buildlinux/makezip64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Before executing this script make sure you have the 64 bit debian package installed.
-cat files.txt | zip safejumper-linux-64.zip -@
-zip -r safejumper-linux-64.zip /opt/safejumper 
+cat files.txt | zip shieldtra-linux-64.zip -@
+zip -r shieldtra-linux-64.zip /opt/shieldtra
